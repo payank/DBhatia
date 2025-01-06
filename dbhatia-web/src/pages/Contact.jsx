42c21e7810f36ef200cfe3b89a2f6497476acbbe
payankjain <payank.jain@globallogic.com>
2025-01-06 09:27:18 +0530

    utilities

--- dbhatia-web/src/pages/Contact.jsx
+++ dbhatia-web/src/pages/Contact.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import contactImage from "../components/assets/images/contact.webp"
+import contactImage from "../components/assets/images/contact1.png"
 
 const SEPARATOR = ':' ;
 
